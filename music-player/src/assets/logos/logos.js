d789c5a231ba145ac3dff5054112cccc9c36442e
Vishnu V S <vishnusenthil222@gmail.com>
2024-01-31 11:22:50 +0530

    music player playlist styled

---
+++ music-player/src/assets/logos/logos.js
@@ -0,0 +1,7 @@
+import enthero from "./enthero.jpg"
+import Kokkarakokkarako from './Kokkarakokkarako.jpg'
+import naaReady from './naaReady.jpg'
+import palaPalakkura from './palaPalakkura.jpeg'
+import UdhundadaSangu from "./UdhundadaSangu.jpg"
+
+export {enthero,Kokkarakokkarako,naaReady,palaPalakkura,UdhundadaSangu}
--- music-player/src/pages/Home.jsx
+++ music-player/src/pages/Home.jsx
@@ -8,22 +8,49 @@ import three from "../assets/music/3.mp3";
 import four from "../assets/music/4.mp3";
 import five from "../assets/music/5.mp3";
 
+import {
+  enthero,
+  Kokkarakokkarako,
+  naaReady,
+  palaPalakkura,
+  UdhundadaSangu,
+} from "../assets/logos/logos";
+
 function Home() {
   const [playlist, setPlaylist] = useState([
-    { name: "Pala Palakkura", music: Ayan, id: 1, composer: "Harris Jayaraj" },
+    {
+      name: "Pala Palakkura",
+      music: Ayan,
+      id: 1,
+      composer: "Harris Jayaraj",
+      img: palaPalakkura,
+    },
     {
       name: "Udhungada Sangu ",
       music: two,
       id: 2,
       composer: "Anirudh Ravichander",
+      img: UdhundadaSangu,
     },
-    { name: "Enthero", music: three, id: 3, composer: "appu" },
-    { name: "Kokkara Kokkarako", music: four, id: 4, composer: "Vidyasagar" },
-    { name: "Naa Ready", music: five, id: 5, composer: "Anirudh Ravichander" },
+    { name: "Enthero", music: three, id: 3, composer: "appu", img: enthero },
+    {
+      name: "Kokkara Kokkarako",
+      music: four,
+      id: 4,
+      composer: "Vidyasagar",
+      img: Kokkarakokkarako,
+    },
+    {
+      name: "Naa Ready",
+      music: five,
+      id: 5,
+      composer: "Anirudh Ravichander",
+      img: naaReady,
+    },
   ]);
 
-  const [counter, setCounter] = useState(0);
-  const [stopCounter, setStopCounter] = useState(true);
+  const [counter, setCounter] = useState();
+  const [isPlaying, setisPlaying] = useState(false);
 
   const showToastMessage = () => {
     toast("Playing next track!", {
@@ -37,23 +64,27 @@ function Home() {
     });
   };
 
-  const counterLoop = async (count) => {
-    for (let i = 0; i < 4; i++) {
+  const increNum = async (i) => {
+    await new Promise((resolve) =>
       setTimeout(() => {
-        console.log(count+1)
-      }, i * 1000);
+        resolve(setCounter(i));
+      }, 1000)
+    );
+  };
+  const theLoop = async () => {
+    for (let i = 1; i <= 4; i++) {
+      await increNum(i);
+    }
+  };
+  const runLoops = async (id) => {
+    selectMusic(id);
+    for (let i = 0; i < 25; i++) {
+      await theLoop();
     }
   };
 
-  const handleCounter = async () => {
-    await new Promise((res, rej) => {
-      for (let i = 1; i <= 3; i++) {
-        setTimeout(() => {
-          counterLoop(i);
-          res();
-        }, i * 1000);
-      }
-    });
+  const selectMusic = (id) => {
+    console.log(id);
   };
 
   return (
@@ -62,18 +93,20 @@ function Home() {
         <h1>Music Player</h1>
       </header>
       <section className="musicContainer">
-        <div className="streaming"></div>
-        {counter}
-        {playlist.map((m) => {
-          return (
-            <div className="music" key={m.id} onClick={handleCounter}>
-              <p>{m.name}</p>
-              <audio controls>
-                <source src={m.music} />
-              </audio>
-            </div>
-          );
-        })}
+        <div id="playStream">
+          <div className="streaming"></div>
+          {playlist.map((m) => {
+            return (
+              <div className="music" key={m.id} onClick={() => runLoops(m.id)}>
+                <img src={m.img} alt="i" id="musicLogo" />
+                <div>
+                  <p id="musicName">{m.name}</p>
+                  <p id="composer">{m.composer}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </section>
       <ToastContainer />
     </div>
