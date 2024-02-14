25bd61c2f025c6c265000d7c2c7259b63a711f85
Vishnu V S <vishnusenthil222@gmail.com>
2024-02-14 18:15:30 +0530

    with key started

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NoKey from './pages/NoKey'
+import WithKey from './pages/WithKey'
 
 function App() {
   return (
     <>
+      <WithKey/>
       <NoKey/>
+
+      
     </>
   )
 }
---
+++ quiz-app/src/pages/WithKey.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
+
+function WithKey() {
+  const [quizs, setQuizs] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState(0);
+  const [selectedQuestions, setSelectedQuestions] = useState([]);
+  const [isComplete, setIsComplete] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      "https://quizapi.io/api/v1/questions?apiKey=cr67FaGJQKBUFZHXI5D2sqBmmjvX0MT2PijGyqmV&category=linux&difficulty=Hard&limit=10"
+    )
+      .then((res) => res.json())
+      .then((ans) => setQuizs(ans));
+  }, []);
+
+  let charArr = ["a", "b", "c", "d", "e", "f"];
+
+  return (
+    <div className="container">
+      <Header />
+      <section id="quizContainer">
+        {quizs.map((q, i) => (
+          <div key={i} id="singleQuestion">
+            <h2>
+              {i + 1} - {q.question}
+            </h2>
+            {/* {console.log(q.answers)} */}
+            {charArr.map((ans, i) => (
+              <>
+                {/* <p>{`${q.answers[0]}${ans}`}</p> */}
+                {console.log()}
+              </>
+            ))}
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default WithKey;
